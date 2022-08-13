b844bad7962178bcb84e9d2464ec308f64312aa3
mnihussain <md.nasiribnehussain@gmail.com>
2022-08-13 00:30:44 -0400

    init

---
+++ src/main/java/indigo/amazon/base/BaseClass.java
@@ -0,0 +1,5 @@
+package indigo.amazon.base;
+
+public class BaseClass {
+
+}
---
+++ src/test/java/indigo/amazon/tests/TestClass.java
@@ -0,0 +1,11 @@
+package indigo.amazon.tests;
+
+import org.testng.annotations.Test;
+
+public class TestClass {
+
+	@Test
+	public void test1() {
+		//version
+	}
+}
